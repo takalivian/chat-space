b1c12f63767a933f9bc935a62961f718f348406e
takalivian <61145524+takalivian@users.noreply.github.com>
2020-02-29 01:44:12 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: [' ~/.ssh/takalivian.pem'] 
+                  keys: [' ~/.ssh/cht-space.pem'] 
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
