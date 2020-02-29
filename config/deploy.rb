9a559edfe79ecb1a6b0938ecc15c4dd3a83e7617
takalivian <61145524+takalivian@users.noreply.github.com>
2020-02-29 19:57:31 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.12.0"
+lock "3.12.0"
 
 set :application, "chat-space"
 set :repo_url, "git@github.com:takalivian/chat-space.git"
